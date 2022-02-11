e83ff8102a1778df81131db9eb0ab212de97398e
Daniel Tejeda <daviddan1998@gmail.com>
2022-02-11 13:31:11 -0500

    Initial Commit, got scraper working and passing data to NodeJS

---
+++ Node/app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('./public'))
+
+app.use(express.json())
+
+app.get('/api/v1/getSchedule', (req, res) =>
+{
+  console.log(`Get schedule called with username and password ${req.body.username} ${req.body.password}!`)
+  const spawn = require("child_process").spawn
+  const pythonProcess = spawn('python', ["../PythonScripts/scraper.py", req.body.username, req.body.password])
+  console.log("PythonProsses started!")
+  getData()
+
+  function getData() {
+    pythonProcess.stdout.on('data', (data) =>
+    {
+      data = data.toString()
+      console.log(data + '\n')
+      //Check if we found the end of the data
+      if(data.includes('ENDOFDATA'))
+      {
+        //Clean up the data
+        data = data.substring(data.indexOf("STARTOFDATA") + 11, data.lastIndexOf("ENDOFDATA"))
+        res.json({msg: data})
+      }
+      else 
+      {
+        //Recursively call getData if ENDOFDATA delimiter is not found
+        getData()
+      }
+    })
+  }
+})
+
+app.get('/test', (req, res) =>
+{
+  console.log("TEST CALLED")
+  res.send("this is a test")
+})
+
+
+app.listen(8001, console.log("server online..."))
---
+++ PythonScripts/scraper.py
@@ -0,0 +1,58 @@
+import sys
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ExpectedConditions
+from selenium.webdriver.remote.webelement import WebElement
+
+passportUrl = 'https://www.publix.org/'
+
+username = sys.argv[1]
+password = sys.argv[2]
+
+chrome_options = Options()
+
+chrome_options.add_argument('--user-agent=Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36')
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+wait = WebDriverWait(driver, 10)
+driver.get(passportUrl)
+
+usernameField = driver.find_element_by_xpath('//*[@id="USER"]')
+usernameField.send_keys(username)
+
+passwordField = driver.find_element_by_xpath('//*[@id="password"]')
+passwordField.send_keys(password)
+
+loginButton = driver.find_element_by_xpath('//*[@id="Login"]')
+loginButton.click()
+
+sleep(1)
+
+scheduleButton = driver.find_element_by_xpath('/html/body/main/div[4]/div/div[2]')
+scheduleButton.click()
+
+sleep(1)
+
+weekData = driver.find_element_by_xpath('//*[@id="scheduledweek"]/div[1]/div[2]/div[1]')
+
+scheduleListHour = []
+scheduleListDay = []
+
+# print(driver.page_source)
+# # Get data and time from each day (7 days total)
+for i in range(1,8):
+  scheduleListHour.append(driver.find_element_by_xpath('//*[@id="accordian"]/a[' + str(i) + ']/div/div[2]/span[1]').get_attribute('innerHTML').strip())
+  scheduleListDay.append(driver.find_element_by_xpath('//*[@id="accordian"]/a[' + str(i) + ']/div/div[1]/div[2]').get_attribute('innerHTML').strip())
+
+driver.quit()
+
+dataToSendBack = ""
+
+for i in range(0,7):
+  dataToSendBack += scheduleListDay[i] + ' : ' + scheduleListHour[i] + '\n'
+
+print("STARTOFDATA\n" + dataToSendBack + " ENDOFDATA")
+sys.stdout.flush()
